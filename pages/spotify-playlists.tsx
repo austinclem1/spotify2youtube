13efeae775ea0a6acf840f16819f7cd4df3b9785
Austin Clements <austinclementsbass@gmail.com>
2021-02-16 09:24:21 -0600

    Add initial playlist card sorting

--- pages/spotify-playlists.tsx
+++ pages/spotify-playlists.tsx
@@ -220,6 +220,18 @@ async function getSpotifyUserPlaylists(accessToken) {
 		}
 	})
 
+	userPlaylists.sort((a, b) => {
+		const nameA = a.name.toUpperCase()
+		const nameB = b.name.toUpperCase()
+		if (nameA < nameB) {
+			return -1
+		}
+		if (nameA > nameB) {
+			return 1
+		}
+		return 0
+	})
+
 	return userPlaylists
 }
 

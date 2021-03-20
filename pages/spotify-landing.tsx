c8fa4bf10378e6663453ca6322e3d0ef0822247d
Austin Clements <austinclementsbass@gmail.com>
2021-03-20 15:23:39 -0500

    Add test loading spinner to spotify-landing

--- pages/spotify-landing.tsx
+++ pages/spotify-landing.tsx
@@ -1,5 +1,35 @@
+import Spinner from "react-bootstrap/Spinner"
+import {
+	getSpotifyAccessToken,
+	getSpotifyUserPlaylists
+} from "../helpers/spotify-helpers"
+import useSWR from "swr"
+
 function SpotifyLanding() {
-	return <p>Hi</p>
+	const { data, error } = useSWR("spotifyUserPlaylists", getSpotifyUserPlaylists)
+
+	return (
+		<div>
+			{data &&
+				data.map((playlist) => {
+					return (
+						<div>
+							<p>{playlist.id}</p>
+							<p>{playlist.name}</p>
+						{playlist.tracks.map((track) => {
+							return (
+								<p>{track.name}</p>
+							)
+						})}
+						</div>)
+				})
+			}
+			{!data &&
+				<Spinner animation="border" />
+			}
+		</div>
+	)
 }
 
 export default SpotifyLanding
+

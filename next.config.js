a33ec6abd890545de1ff1fb5e07d9f823224ec25
Austin Clements <austinclementsbass@gmail.com>
2021-03-20 15:20:56 -0500

    Make new client secrets and hide them in this repo

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const spotifyClientId = 'e9d00f9dd4f842e5af42ede328df4de7'
-const spotifyClientSecret = '852e05198509492195cd23f45c173c4d'
+const spotifyClientId = "e9d00f9dd4f842e5af42ede328df4de7"
+const spotifyClientSecret = "REDACTED FOR GITHUB"
 const spotifyAuthorizationBuffer = Buffer.from(
-	spotifyClientId + ':' +
+	spotifyClientId + ":" +
 	spotifyClientSecret,
-	'utf-8'
+	"utf-8"
 )
-const spotifyTokenAuthorization = spotifyAuthorizationBuffer.toString('base64')
+const spotifyTokenAuthorization = spotifyAuthorizationBuffer.toString("base64")
 
 module.exports = {
 	env: {
@@ -13,8 +13,8 @@ module.exports = {
 		spotifyClientSecret,
 		spotifyTokenAuthorization,
 		spotifyReducedTrackCount: 5,
-		youtubeClientId: '543052497909-vovj9kgvse95tg5ufseoc1go31cidfnl.apps.googleusercontent.com',
-		youtubeClientSecret: 'OU89BpNFpZynNK4PPE8mkwZD',
-		youtubeApiKey: 'AIzaSyBAL6pe6co6_6Ljy20ZXzvtc9XXxMpe1TU',
+		youtubeClientId: "79554421085-160qsevi6kong1b87khn9ah3sfr773k5.apps.googleusercontent.com",
+		youtubeClientSecret: "REDACTED FOR GITHUB",
+		youtubeApiKey: "AIzaSyBAL6pe6co6_6Ljy20ZXzvtc9XXxMpe1TU",
 	}
 }
